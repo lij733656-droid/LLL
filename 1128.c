d191f8d17c3fe2907c1f0d9d62915fc31bc47ce1
lij733656-droid <lij733656@gmail.com>
2026-09-17 19:22:27 +0800

    添加第一个C语言程序

---
+++ 1128.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("Data type        Number of bytes\n");
+    printf("short int        %d\n", sizeof(short));
+    printf("long int         %d\n", sizeof(long));
+    printf("long long int    %d\n", sizeof(long long));
+    printf("float            %d\n", sizeof(float));
+    printf("double           %d\n", sizeof(double));
+    printf("long double      %d\n", sizeof(long double));
+    return 0;
+}
